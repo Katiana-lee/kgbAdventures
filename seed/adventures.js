5d781c8faf2a05a5d3ae86abcfe939666c9943a5
Katiana <katiana002@gmail.com>
2021-02-11 12:17:13 -0500

    changes to seed file

--- seed/adventures.js
+++ seed/adventures.js
@@ -6,14 +6,31 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 const main = async () => {
   const adventures = [
     {
-      title: "Mesmerizing Waterfall",
+      title: "Forest, Beach",
       location: "McArthur-Burney Falls, California",
-      category: ['Forest'],
+      category: ['Forest', 'Beach'],
       price: "$10 per car entry",
       description: "Burney Falls Loop Trail is a 1 mile heavily trafficked loop trail located near Burney, California that features a beautiful waterfall ",
       details: ['hiking', 'horseback-riding','wildlife','camping','fishing'],
       imgURL: ['https://californiathroughmylens.com/wp-content/uploads/2012/06/Burney-Falls.jpg', 'https://i.redd.it/wyaabe8wwvi31.jpg', 'https://i.pinimg.com/originals/0a/97/22/0a972239e235ebf81cc73d3a3e763681.jpg'],
-     
+    },
+    {
+      title: "countryside, desert",
+      location: "Nomad Land",
+      category: ['Country', 'Desert'],
+      price: "FREE",
+      description: "Lovely view of the sunset with a special someone. The view is to die for!! ",
+      details: ['hiking', 'boatride','camping'],
+      imgURL: ['', '', ''],
+    },
+    {
+      title: "forest",
+      location: "random",
+      category: ['random'],
+      price: "FREE",
+      description: "",
+      details: [''],
+      imgURL: ['', '', ''],
     }
   ];
 
