0245f426a416a051cd864e5d3dc2b5830d65db5a
Grady King <grady.king07@gmail.com>
2021-02-14 18:24:40 -0500

    forms finally work

--- client/src/screens/AdventureCreate/Create.jsx
+++ client/src/screens/AdventureCreate/Create.jsx
@@ -6,7 +6,19 @@ import { useEffect } from 'react'
 import { createAdventure } from '../../services/adventures'
 
 const AdventureCreate = (props) => {
+  const [d, setD] = useState({
+    detail1: '',
+    detail2: '',
+    detail3: '',
+  })
 
+  const [images, setImages] = useState({
+    image1: '',
+    image2: '',
+    image3: '',
+  })
+  
+  
  
 
 
@@ -16,9 +28,11 @@ const AdventureCreate = (props) => {
     category: [],
     price: '',
     description: '',
-    details: ["", "", ""],
-    imgURL: ["", "", ""]
+    details: [],
+    imgURL: []
   })
+
+ 
 
   const [isCreated, setCreated] = useState(false)
 
@@ -38,10 +52,36 @@ const AdventureCreate = (props) => {
     })
     adventure.category.push(event.target.value)
   }
+
+
+
+  const detailsChange = (event) => {
+    const { name, value } = event.target
+    setD({
+      ...d,
+      [name]:value
+    })
+  }
+
+  const imagesChange = (event) => {
+    const { name, value } = event.target
+    setImages({
+      ...images,
+      [name]:value
+    })
+  }
+  
   
 
   const handleSubmit = async (event) => {
     event.preventDefault()
+    let imagesToAdd = [images.image1,images.image2,images.image3]
+    let detailsToAdd = [d.detail1,d.detail2,d.detail3]
+    setAdventure({
+      ...adventure,
+      details: detailsToAdd,
+      imgURL: imagesToAdd
+})
     const created = await createAdventure(adventure)
     setCreated({ created })
   }
@@ -88,7 +128,7 @@ const AdventureCreate = (props) => {
           className='input-category-1'
           value={adventure.category}
           name= 'input-category-1'
-          required
+          // required
         onChange={arrayChange}>
             <option value="" disable selection hidden>Select Category</option>
             <option value="Beach">Beach</option>
@@ -107,7 +147,7 @@ const AdventureCreate = (props) => {
           className='input-category-2'
           value={adventure.category}
           name='input-category-2'
-          required
+          // required
         onChange={arrayChange}>
             <option value="" disable selection hidden>Select Category</option>
             
@@ -127,7 +167,7 @@ const AdventureCreate = (props) => {
           className='input-category-3'
           value={adventure.category}
           name='input-category-3'
-          required
+          // required
         onChange={arrayChange}>
             <option value="" disable selection hidden>Select Category</option>
             <option value="Beach">Beach</option>
@@ -195,54 +235,73 @@ const AdventureCreate = (props) => {
         {/* Details */}
         <label for="input-details">details:</label>
         <input
-          id="input-details"
+          type='text'
+          id="input-details-1"
           className="input-details details-1"
           placeholder='Enter Details'
-          value={adventure.details[0]}
-          name='details-1'
+          value={d.detail1}
+          name='detail1'
           required
-          onChange={handleChange}
+          onChange={detailsChange}
         />
+
+
         <input
+          id='input-details-2'
           className="input-details details-2"
           placeholder='Enter Details'
-          value={adventure.details[1]}
-          name='details-2'
-          onChange={handleChange}
+          value={d.detail2}
+          name='detail2'
+          required
+          onChange={detailsChange}
         />
         <input
+          id='input-details-3'
           className="input-details details-3"
           placeholder='Enter Details'
-          value={adventure.details[2]}
-          name='details-3'
-          onChange={handleChange}
+          value={d.detail3}
+          name='detail3'
+          required
+          onChange={detailsChange}
         />
 
         {/* Images */}
-        <label for="input-img">imgs:</label>
+        <label for="input-images">details:</label>
         <input
-          id="input-img"
-          className="input-img imgURL-1"
-          placeholder='Enter Image URL'
-          value={adventure.imgURL[0]}
-          name='imgURL-1'
+          type='text'
+          id="input-images-1"
+          className="input-images images-1"
+          placeholder='Enter Images'
+          value={images.imgage1}
+          name='image1'
           required
-          onChange={handleChange}
+          onChange={imagesChange}
         />
-        <input
-          className="input-imgs imgURL-2"
-          placeholder='Enter Image URL'
-          value={adventure.imgURL[1]}
-          name='imgURL-2'
-          onChange={handleChange}
+
+
+<input
+          type='text'
+          id="input-images-2"
+          className="input-images images-2"
+          placeholder='Enter Images'
+          value={images.imgage2}
+          name='image2'
+          required
+          onChange={imagesChange}
         />
-        <input
-          className="input-imgs imgURL-3"
-          placeholder='Enter Image URL'
-          value={adventure.imgURL[2]}
-          name='imgURL-3'
-          onChange={handleChange}
+<input
+          type='text'
+          id="input-images-3"
+          className="input-images images-3"
+          placeholder='Enter Images'
+          value={images.imgage3}
+          name='image3'
+          required
+          onChange={imagesChange}
         />
+
+
+        
         <button type='submit' className="submit-button">Submit</button>
       </form>
     </Layout>
