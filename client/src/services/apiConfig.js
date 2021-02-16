660f24ba1d592a36308459ae54090e7ca552327c
Katiana <katiana002@gmail.com>
2021-02-16 12:56:46 -0500

    everything is working

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -11,9 +11,9 @@ const getToken = () => {
 
 const api = axios.create({
     baseURL: process.env.NODE_ENV === 'production'
-    ? 'https://sei-adventures-api.herokuapp.com/api'
-    // : 'https://sei-adventures-api.herokuapp.com/api'
-        : 'http://localhost:3000/api'
+    ? 'https://kgb-adventures.herokuapp.com/api'
+    : 'https://kgb-adventures.herokuapp.com/api'
+        // : 'http://localhost:3000/api'
 })
 
 
