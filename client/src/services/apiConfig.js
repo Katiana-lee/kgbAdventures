d91b8d230589f46af292a798da043d504fd16385
Katiana <katiana002@gmail.com>
2021-02-16 10:59:57 -0500

    changes to apiConfug file

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -12,8 +12,8 @@ const getToken = () => {
 const api = axios.create({
     baseURL: process.env.NODE_ENV === 'production'
     ? 'https://sei-adventures-api.herokuapp.com/api'
-    // : 'https://sei-adventures-api.herokuapp.com/api'
-        : 'http://localhost:3000/api'
+    : 'https://sei-adventures-api.herokuapp.com/api'
+        // : 'http://localhost:3000/api'
 })
 
 
