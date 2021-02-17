3687be351057f19da6380800ac9b58af1eae32d9
benazevedo <ben_azevedo@outlook.com>
2021-02-17 04:43:13 -0800

    detail done minus photo function

--- client/src/screens/AdventureDetail/Detail.jsx
+++ client/src/screens/AdventureDetail/Detail.jsx
@@ -9,38 +9,72 @@ const AdventureDetail = (props) => {
   const [adventure, setAdventure] = useState(null)
   const [isLoaded, setLoaded] = useState(false)
   const { id } = useParams()
+  const [mainImage, setMainImage] = useState("");
 
   useEffect(() => {
       const fetchAdventure = async () => {
           const adventure = await getAdventure(id)
           setAdventure(adventure)
-          setLoaded(true)
+        setLoaded(true)
+        setMainImage(adventure.imgURL[0])
       }
       fetchAdventure()
   }, [id])
 
   if (!isLoaded) {
-      return <h1>Loading...</h1>
+      return <h1 className="loader">Loading...</h1>
   }
+
+  const detailList = adventure.details.map((detail, index) => (
+    <li className="detail-single">
+      {detail}
+    </li>
+  ));
+
+  const switcheroo = (e, image) => {
+    e.preventDefault();
+    console.log(image);
+    setMainImage(image);
+  }
+
+  const imageList = adventure.imgURL.map((image, index) => (
+    <img className="image-single" onClick={ (e) => switcheroo(e, {image}) } src={image}/>
+  ));
 
   return (
     <Layout>
       <div className="adventure-detail">
         <div className="detail-content">
           <div className="detail-title">{adventure.title}</div>
-          <div className="detail-location">{adventure.location}</div>
-          <div className="detail-price"><i class="fa fa-rub" aria-hidden="true"></i> {adventure.price}</div>
-          <div className="detail-description">{adventure.description}</div>
-
+          <div className="detail-location"><i class="fa fa-map-marker" aria-hidden="true"></i> {adventure.location}</div>
+          <br></br>
+          <div className="detail-price-block">
+            <div className="detail-price"><i class="fa fa-rub" aria-hidden="true"></i> {adventure.price}</div>
+            <div className="add-to-cart">ADD TO CART</div>
+          </div>
+          <div className="detail-description">
+            <span className="span-title">Description:</span>
+            <br></br>
+            <p>{adventure.description}</p>
+          </div>
+          <div className="detail-details">
+            <span className="span-title">Details:</span>
+            <ul>{detailList}</ul>
+          </div>
           <div className="button-container">
             <button className="edit-button"><Link className="edit-link" to={`/adventures/${adventure._id}/edit`}>Edit</Link></button>
             <button className="delete-button" onClick={() => deleteAdventure(adventure._id)}>Delete</button>
           </div>
         </div>
+        <div className="detail-center-divider"></div>
         <div className="detail-images">
-          <img className="adventure-detail-image" src={adventure.imgURL[0]} alt={adventure.title} />
+          <img className="adventure-detail-image" src={mainImage} alt={adventure.title} />
+          <div className="other-images">
+            {imageList}
+          </div>
         </div>
       </div>
+      <div className="fake-footer"></div>
     </Layout>
   )
 }
