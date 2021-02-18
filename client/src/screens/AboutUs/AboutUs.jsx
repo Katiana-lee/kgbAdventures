cb30c46e86f3904ee364a172fc8641d75e577db6
benazevedo <ben_azevedo@outlook.com>
2021-02-18 09:55:55 -0800

    nav and footer complete

--- client/src/screens/AboutUs/AboutUs.jsx
+++ client/src/screens/AboutUs/AboutUs.jsx
@@ -1,107 +1,92 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Layout from "../../components/shared/Layout/Layout";
+import kEagle from "../../images/kEagle.png";
+import gEagle from "../../images/gEagle.png";
+import bEagle from "../../images/bEagle.png";
 import "./AboutUs.css";
 
 function AboutUs(props) {
   return (
     <Layout onChange={props.onChange}>
       <div class="about-section">
-        <h1>About Us</h1>
-        <p>Some text about who we are and what we do.</p>
+        <h1>Meet the Comrades</h1>
       </div>
-
-      <div className="row">
+      <div className="about-us-container">
         <div className="column">
-          <div className="card">
-            <img
-              className="image"
-              src="https://ca.slack-edge.com/T0351JZQ0-U01F58DPXCP-78022d5ef928-512"
-              alt="Ben"
-            />
-            <div className="container">
-              
-              <h2>Ben A</h2>
-              <p className="title">Software Engineer</p>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia
-                iste nemo dignissimos, veritatis id asperiores qui eius. Maxime
-                aut, expedita consequatur non suscipit, laudantium ab sed ut,
-                sit iusto quibusdam?
-              </p>
-             <div className="icons">
-              <a className="github" href="https://github.com/ben-azevedo/" target="_blank">
-              <i class="fa fa-github" aria-hidden="true"></i>
-              </a>
-              <a className="linkedin" href="https://www.linkedin.com/in/grady-king/" target="_blank">
-                <i class="fa fa-linkedin" aria-hidden="true"></i>
-              </a>
-</div>
-              <p>
-                {/* <button className="button">Contact</button> */}
-              </p>
+            <div className="card">
+              <img
+                className="image"
+                src={kEagle}
+                alt="Katiana"
+              />
+              <div className="container">
+                <h2>Katiana L</h2>
+                <p className="title">Software Engineer</p>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum
+                  reprehenderit cum voluptatum asperiores voluptates, excepturi
+                  optio laboriosam recusandae dolorem totam a similique dolor quis
+                  reiciendis maxime iste voluptate laborum commodi.
+                </p>
+                <div className="icons">
+                  <a className="github"href="https://github.com/Katiana-lee/" target="_blank">
+                    <i class="fa fa-github" aria-hidden="true"></i>
+                  </a>
+                  <a className="linkedin"href="https://www.linkedin.com/in/grady-king/" target="_blank">
+                    <i class="fa fa-linkedin" aria-hidden="true"></i>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="column">
-          <div className="card">
-            <img
-              className="image"
-              src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png"
-              alt="Grady"
-            />
-            <div className="container">
-              <h2>Grady K</h2>
-              <p className="title">Software Engineer</p>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Dolorum distinctio ducimus provident repellendus qui maiores
-                quae vero aspernatur, aperiam ratione aliquid ipsum consequatur
-                illum! Deleniti voluptatum temporibus quaerat atque sapiente.
-              </p>
-              <div className="icons">
-              <a className="github"href="https://github.com/gradyki/" target="_blank">
-                <i class="fa fa-github" aria-hidden="true"></i>
-              </a>
-              <a className="linkedin"href="https://www.linkedin.com/in/grady-king/" target="_blank">
-                <i class="fa fa-linkedin" aria-hidden="true"></i>
-                </a>
+          <div className="column">
+            <div className="card">
+              <img className="image" src={gEagle} alt="Grady"/>
+              <div className="container">
+                <h2>Grady K</h2>
+                <p className="title">Software Engineer</p>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum
+                  reprehenderit cum voluptatum asperiores voluptates, excepturi
+                  optio laboriosam recusandae dolorem totam a similique dolor quis
+                  reiciendis maxime iste voluptate laborum commodi.
+                </p>
+                <div className="icons">
+                  <a className="github"href="https://github.com/gradyki/" target="_blank">
+                    <i class="fa fa-github" aria-hidden="true"></i>
+                  </a>
+                  <a className="linkedin"href="https://www.linkedin.com/in/grady-king/" target="_blank">
+                    <i class="fa fa-linkedin" aria-hidden="true"></i>
+                  </a>
                 </div>
-              <p>
-                {/* <button className="button">Contact</button> */}
-              </p>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="column">
-          <div className="card">
-            <img
-              className="image"
-              src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png"
-              alt="Katiana"
-            />
-            <div className="container">
-              <h2>Katiana L</h2>
-              <p className="title">Software Engineer</p>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum
-                reprehenderit cum voluptatum asperiores voluptates, excepturi
-                optio laboriosam recusandae dolorem totam a similique dolor quis
-                reiciendis maxime iste voluptate laborum commodi.
-              </p>
-              <div className="icons">
-              <a className="github"href="https://github.com/Katiana-lee/" target="_blank">
-              <i class="fa fa-github" aria-hidden="true"></i>
-              </a>
-              <a className="linkedin"href="https://www.linkedin.com/in/grady-king/" target="_blank">
-                <i class="fa fa-linkedin" aria-hidden="true"></i>
-                </a>
+        
+        <div className="row">
+          <div className="column">
+            <div className="card">
+              <img className="image" src={bEagle} alt="Ben" />
+              <div className="container">
+                <h2>Ben A</h2>
+                <p className="title">Software Engineer</p>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum
+                  reprehenderit cum voluptatum asperiores voluptates, excepturi
+                  optio laboriosam recusandae dolorem totam a similique dolor quis
+                  reiciendis maxime iste voluptate laborum commodi.
+                </p>
+                <div className="icons">
+                  <a className="github" href="https://github.com/ben-azevedo/" target="_blank">
+                    <i class="fa fa-github" aria-hidden="true"></i>
+                  </a>
+                  <a className="linkedin" href="https://www.linkedin.com/in/grady-king/" target="_blank">
+                    <i class="fa fa-linkedin" aria-hidden="true"></i>
+                  </a>
                 </div>
-              <p>
-                {/* <button className="button">Contact</button> */}
-              </p>
+              </div>
             </div>
           </div>
         </div>
--- client/src/screens/ShoppingCart/ShoppingCart.jsx
+++ client/src/screens/ShoppingCart/ShoppingCart.jsx
@@ -5,30 +5,36 @@ import { Link } from "react-router-dom";
 function ShoppingCart(props) {
   const { user, onChange, cart, setCart } = props;
   console.log(cart);
+  let total = 0;
+
+  const shoppingTotal = cart.map((adventure, index) => {
+    total += parseInt(adventure.price);
+  })
 
   const shoppingList = cart.map((adventure, index) => (
     <div className="adventure-shop">
+      <img className="adventure-shop-image" src={adventure.imgURL[0]} alt={adventure.title} />
       <div className="shop-content">
         <div className="shop-title">{adventure.title}</div>
         <div className="shop-location"><i class="fa fa-map-marker" aria-hidden="true"></i> {adventure.location}</div>
-        <br></br>
-        <div className="shop-price-block">
-          <div className="shop-price"><i class="fa fa-rub" aria-hidden="true"></i> {adventure.price}</div>
-          {/* <div className="add-to-cart" onClick={(e) => buildCart(e)}>ADD TO CART</div> */}
-        </div>
       </div>
-      <div className="shop-center-divider"></div>
-      <div className="shop-images">
-        <img className="adventure-shop-image" src={adventure.imgURL[0]} alt={adventure.title} />
+      <div className="shop-price-block">
+        <div className="shop-price"><i class="fa fa-rub" aria-hidden="true"></i> {adventure.price}</div>
       </div>
     </div>
   ));
 
   return (
-    <Layout onChange={ onChange } user={props.user}>
-      <div>Shopping Cart</div>
-      <div>
+    <Layout className="shop-layout" onChange={ onChange } user={props.user}>
+      <div className="fake-nav"></div>
+      <div className="shopping-title" style={{paddingTop: "5vh"}} >Shopping Cart</div>
+      <div className="shop-container">
+        <div></div>
         {shoppingList}
+        <div className="total-price">
+          <i class="fa fa-rub" aria-hidden="true"></i>
+          {shoppingTotal}
+        </div>
       </div>
       <div className="fake-footer"></div>
     </Layout>
