942cb9d807c19bb223faae476382d0bf457de1ca
Grady King <grady.king07@gmail.com>
2021-02-18 09:11:07 -0500

    CarouselUpdates,  utils file for data, links added to details

--- client/src/components/LandingCarousel/LandingCarousel.jsx
+++ client/src/components/LandingCarousel/LandingCarousel.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Carousel from 'react-elastic-carousel'
+import carouselData from '../../utils/carouselData'
+import { Link } from 'react-router-dom'
 import './LandingCarousel.css';
 
 
@@ -16,32 +18,22 @@ const breakPoints = [
 
 class LandingCarousel extends Component{
   state = {
-    images: [
-      { id: 1, url: 'https://scontent.cdninstagram.com/v/t51.2885-15/e35/p480x480/150094779_739341800287336_1723276445443906731_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=lBNXctOfZC4AX-JOrb8&tp=1&oh=2d7f39169273ecba280898d4281cbe8a&oe=60553FA7', textLocation: 'Tours of Italian Coast', textPrice: '8000'},
-      { id: 2, url: 'https://scontent.cdninstagram.com/v/t51.2885-15/e35/p480x480/37183915_230688687587919_1296630019409313792_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=iN6Wj62qnQAAX8xpio_&tp=1&oh=268d18b75ff8a8797d01d574f5a835ac&oe=60572130', textLocation: 'Alpine Cabins, Slovenia', textPrice: '30,000/night'},
-      { id: 3, url: 'https://scontent.cdninstagram.com/v/t51.2885-15/e35/p480x480/35575396_364462950744813_5202827729157750784_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=109&_nc_ohc=Dy6eQgD01O0AX-RS6vW&tp=1&oh=154dbbfca60503c9565f59d30521c2b9&oe=6058640F', textLocation: 'Private Surfing Lessons', textPrice: '7500'},
-      { id: 4, url: 'https://scontent.cdninstagram.com/v/t51.2885-15/e15/p480x480/30855499_192914748005624_1782026723324329984_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=108&_nc_ohc=CxaZfgQi7aIAX8sZcvu&tp=1&oh=3a2bda3a0f4892a1e6e3a757a898373c&oe=605530F0', textLocation: 'Forest Cabins, Switzerland', textPrice: '$25000/night'},
-      { id: 5, url: 'https://scontent.cdninstagram.com/v/t51.2885-15/e35/p480x480/91894206_519604932327851_6855248686194385939_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=111&_nc_ohc=s9SqZMlhxfcAX_-3_J1&tp=1&oh=5254240ec49cbd90d2118331a9f0edbd&oe=6055A189', textLocation: 'Tropical Tree Houses', textPrice: '20,000/night'},
-      { id: 6, url: 'https://scontent.cdninstagram.com/v/t51.2885-15/e35/p480x480/41673078_344458579454532_7420573419078806893_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=100&_nc_ohc=38Op4QEuPrAAX9PjMmi&tp=1&oh=33670a40fdc746279227763e86573bf2&oe=605608F4', textLocation: 'Kyoto Boat Tours', textPrice: '5000'},
-      { id: 7, url: 'https://scontent.cdninstagram.com/v/t51.2885-15/e35/p480x480/89225227_562164671315859_5036329430875035294_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=E_XqBysUC0kAX8ejRHL&tp=1&oh=dfb79fe7cd01f0a85b4bdc52c481bbbd&oe=60561735', textLocation: 'Alp Jeep Rentals', textPrice: '10,000/day'},
-      { id: 8, url: 'https://scontent.cdninstagram.com/v/t51.2885-15/e35/p480x480/88204883_284142452550904_1113199196666979135_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=-zFkSA1iCnMAX_yMym9&tp=1&oh=9c4a2ba564adf160749d27a4b5d875b2&oe=60580E3D', textLocation: 'Appalachian Trail Tours', textPrice: '2000'},
-      { id: 9, url: 'https://scontent.cdninstagram.com/v/t51.2885-15/e35/p480x480/87226071_186591489283795_5990734799709358675_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=107&_nc_ohc=aV7cTFfPJS0AX9qQ7sc&tp=1&oh=6ed270d0a78df9716fb3264185682488&oe=605684D2', textLocation: 'Bora Bora Bungalows', textPrice: '50000/night'},
-      { id: 10, url: 'https://scontent.cdninstagram.com/v/t51.2885-15/e35/p480x480/77425998_279451822986184_3372904222943416965_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=uJFXq1BzGtwAX8Z6Krr&tp=1&oh=b5362592257d4ec2720eb0a57968f86f&oe=60561466', textLocation: 'Taj-Mahal Yoga Classes', textPrice: '3500'},
-      
-    ]
+    images: carouselData
   }
-
+ 
   
   render() {
     const { images } = this.state
     
     return (
         <Carousel breakPoints={breakPoints}>
-          {images.map(image => <div key={image.id} className='image-container'>
+        {images.map(image => <div key={image.id} className='image-container'>
+            <Link to={`/adventures/${image.address}`}> 
             <img src={image.url} id='carousel-imgs' />
             <div className='image-text'>{image.textLocation}
               <br>
               </br><i class="fa fa-rub" aria-hidden="true"></i>{image.textPrice}</div>
+          </Link>    
           </div>)}
         </Carousel>
       )
